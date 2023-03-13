d9cd5061aafe2397cf967bb4b3320e56aa6ca7bc
deevin3 <oviedodivine@gmail.com>
2023-03-13 03:07:48 -0700

    first commit

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print nmaes defined by compiled module hidden_4."""
+    import hidden_4
+
+    names =dir(hidden_4)
+    for name in name:
+        if name[:2]!= "__".
+        print(name)
