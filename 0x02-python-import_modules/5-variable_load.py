150fc0dce9aff91fcc8e48ca36abcdc5d1dc4bac
Deevin3 <117818205+Deevin3@users.noreply.github.com>
2023-03-14 11:31:14 +0100

    first commit

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,6 +2,6 @@
 
 if __name__ = "__main__":
     """Print the value of variable a from 5-variable_load."""
-    import a from 5-variable_load
+    from variable_load_5 import a
     
       print(a)
