680962b3c7eda7935f71259b61fff76130ac1d0b
Deevin3 <117818205+Deevin3@users.noreply.github.com>
2023-05-18 16:22:01 +0100

    first commit

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
- -- Inserts a new row into the table first_table in my MySQL server.
+-- a script that inserts a new row in the table first_table in your MySQL server.
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
