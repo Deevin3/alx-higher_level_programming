2874a202507be6586c8a733af1d81eb9ca0f7c94
Deevin3 <117818205+Deevin3@users.noreply.github.com>
2023-06-06 12:57:16 +0100

    first commit

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,5 @@
---Write a script that uses the hbtn_0d_tvshows database to list all genres not linked to the show DexterSELECT name
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+-- uses a database to list all rows not linked to one row
 SELECT name
 FROM tv_genres
 WHERE name NOT IN
